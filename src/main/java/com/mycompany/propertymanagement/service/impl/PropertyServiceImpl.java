ac5e364a7bf350bda0116ad3a3eb7653b0458b2e
prashantgurung <grgprashant95@gmail.com>
2023-12-05 15:22:36 -0600

    implemented user registration functionality

--- src/main/java/com/mycompany/propertymanagement/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/mycompany/propertymanagement/service/impl/PropertyServiceImpl.java
@@ -61,8 +61,6 @@ public class PropertyServiceImpl implements PropertyService {
             PropertyEntity pe = optEn.get(); //data from db
             pe.setTitle(propertyDTO.getTitle());
             pe.setAddress(propertyDTO.getAddress());
-            pe.setOwnerEmail(propertyDTO.getOwnerEmail());
-            pe.setOwnerName(propertyDTO.getOwnerName());
             pe.setPrice(propertyDTO.getPrice());
             pe.setDescription(propertyDTO.getDescription());
             dto = propertyConverter.convertEntityToDTO(pe);
